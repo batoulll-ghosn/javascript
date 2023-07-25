e973dc471b166bbc3b5a8be431d43ff3009c4836
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-07-25 20:21:50 +0300

    task three done

---
+++ task_3/task_3.js
@@ -0,0 +1,16 @@
+const num1=12;
+var num2=123;
+console.log(num2);
+var canPass=true;
+if(canPass){
+        if(num1>=num2){
+            console.log('Hello!');
+        } else{
+            console.log('Bye');
+        }
+        }
+        else{
+            console.log('Try Again!');
+        }
+num2=7;
+console.log('new value for num2'+ num2);
