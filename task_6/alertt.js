f2f942940841021c0eaacd1497ad795b54c4733e
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-07-26 23:17:22 +0300

    mira

---
+++ task_6/alertt.js
@@ -0,0 +1,9 @@
+function clickedOne(value){
+     selectedButton= value;
+}
+function submitClicked(){
+    updateOutput();
+}
+function updateOutput(){
+    alert('The number choice is: '+selectedButton);
+}
