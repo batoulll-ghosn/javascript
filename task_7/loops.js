9bccac382159ef2b82a88e9a969d65f0c3656df2
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-07-27 10:23:11 +0300

    Done with task 7

---
+++ task_7/loops.js
@@ -0,0 +1,11 @@
+for (let i=1 ;i<=20;i++){
+    if(i%3==0){
+        console.log( i+' '+ 'divi 3')
+    }
+    else if(i%5==0){
+        console.log( i+' '+ 'divi 5')
+    }
+    else  if(i%3==0 && i%5==0){
+        console.log( i+' '+ 'divi 3')
+    }
+}
